9aee0313b30d9bf1f2c930fbb69528ba1d2af43f
Joey Mercia <47761987+joeymercia@users.noreply.github.com>
2025-03-07 14:20:24 -0700

    cleaning up repo

--- themes/assets/js/build/design-pack-notice.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'd04800728e83b9eb3297');
--- themes/assets/js/build/welcome-notice.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '1cb4947c1ddbe5b821b0');
